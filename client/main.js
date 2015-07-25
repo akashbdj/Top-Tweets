458c7c6854645d463e5e305963d1ce267f3d2fc3
Akash Bhardwaj <akashbdj@gmail.com>
2015-07-26 00:29:13 +0530

    show country name and few changes

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 (function(){
 
-  /* Make connection request and connect to server*/
+/*
+    Make connection request and connect to server
+*/
   var socket = io.connect("http://localhost:4000");
   var coordinates = [];
 
@@ -12,27 +14,50 @@
   });
 
 
-  /*  World Map */
 
-  var width   = 1440,
-      height  = 1000;
+/*
+    World Map!
+*/
 
-  var svg = d3.select("body").append("svg")
-              .attr("width", width)
-              .attr("height", height);
+  var width  = 3500,
+      height = 3000;
 
-  var projection  = d3.geo.mercator().scale(160).translate([width/2, height/2]);
+  var canvas = d3.select("body").append("svg")
+               .attr("width", width)
+               .attr("height", height);
+
+/*
+  It shows the type of projection of the map,
+  we're using 'mercator' which is kind of FLAT REPRESTATION
+*/
+  var projection  = d3.geo.mercator().scale(500).translate([width/2, height/2]);
   var path        = d3.geo.path().projection(projection);
 
-  svg.append("rect").attr('width', width).attr('height', height).attr("fill", "#404a67");
+  d3.json("countries.topo.json", function(countries){
 
-  d3.json("world.json", function(error, world) {
-    if (error) return console.error(error);
+   var countriesData = topojson.feature(countries, countries.objects.countries).features;
 
-    var worldData = topojson.feature(world, world.objects.world);
-    svg.append("path").datum(worldData).attr("d", path).attr("fill", "#8a6886").attr("stroke", "#775e7e");
+   var group = canvas.selectAll("g")
+                      .data(countriesData)
+                      .enter()
+                      .append("g");
 
+/*
+  add class to each country path and class name = country name
+*/
+  group.append("path")
+        .attr("class", function(d) { return d.properties.name; })
+        .attr("fill", "#8a6886")
+        .attr("d", path);
+
+/*
+  Show country names on the map
+*/
+  group.append("text")
+        .attr("x", function(d){ return path.centroid(d)[0];})
+        .attr("y", function(d){ return path.centroid(d)[1];})
+        .attr("fill", "#fff")
+        .text(function(d) { return d.properties.name; });
   });
-
 
 }());
