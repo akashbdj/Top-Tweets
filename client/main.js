e8d2b7dd0e9589512b4076dce9b49e0611b35311
Akash Bhardwaj <akashbdj@gmail.com>
2015-07-25 16:20:47 +0530

    Generate map using D3 and GeoJSON

--- client/main.js
+++ client/main.js
@@ -1,6 +1,37 @@
 (function(){
+
+  /* Make connection request and connect to server*/
   var socket = io.connect("http://localhost:4000");
+  var coordinates = [];
+
   socket.on("Connected_start_stream", function(){
     socket.emit("Start_Streaming");
   });
+  socket.on("Sending_Coordinates", function(abc){
+    coordinates.push(abc);
+  });
+
+
+  /*  World Map */
+
+  var width   = 1000,
+      height  = 800;
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  d3.json("world.json", function(error, world) {
+    if (error) return console.error(error);
+
+    var worldData = topojson.feature(world, world.objects.world);
+    var projection = d3.geo.mercator().scale(100).translate([width/2, height/2]);
+
+    var path = d3.geo.path().projection(projection);
+
+    svg.append("path").datum(worldData).attr("d", path).attr("fill", "steelblue").attr("stroke", "#315b7d");
+
+  });
+
+
 }());
--- index.js
+++ index.js
@@ -9,22 +9,22 @@ var server      = new Hapi.Server();
 var twitter = new Twitter({
   consumer_key: 'Your Consumer Key',
   consumer_secret: 'Your Consumer Secret Key ',
-  access_token_key: 'You Acces Token Key',
+  access_token_key: 'You Access Token Key',
   access_token_secret: 'You Access Token Secret Key'
 });
 
 
 /*
-    Connect server to listen at port 4000
-    http://localhost:4000
+  Connect server to listen at port 4000
+  http://localhost:4000
 */
 server.connection({host: 'localhost', port: 4000});
 
 
 /*
-    'io' is a new instance of socket.io.
-    Listener is the node HTTP server
-    object for this connection.
+  'io' is a new instance of socket.io.
+  Listener is the node HTTP server
+  object for this connection.
 */
 var io = require('socket.io')(server.listener);
 
@@ -39,9 +39,14 @@ io.on('connection', function(socket){
   socket.emit("Connected_start_stream");
   socket.on('Start_Streaming', function(){
 
-    twitter.stream('statuses/sample',  function(stream){
+    /* Location filter for the whole world: '-180, -90, 180, 90' */
+    twitter.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream){
       stream.on('data', function(tweet){
-        if(tweet.coordinates !== null){ console.log(tweet.coordinates); }
+
+        if(tweet.coordinates && tweet.coordinates !== null){
+          console.log(tweet.coordinates);
+          socket.emit('Sending_Coordinates', tweet.coordinates);
+        }
       });
 
       stream.on('error', function(error) {
